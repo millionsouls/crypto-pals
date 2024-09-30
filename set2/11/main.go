73e3f2fd024b4d240a32aded90d8a2ef0625c8f7
Kevin W <36448536+millionsouls@users.noreply.github.com>
2024-09-30 14:43:37 -0400

    s2 s11

---
+++ set2/11/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crypto-pals/util"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+
+func GenerateRandomBytes(n int) []byte {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return b
+}
+
+func encryptionOracle(input []byte) []byte {
+	var out []byte
+	choice, err := rand.Int(rand.Reader, big.NewInt(2))
+	key := GenerateRandomBytes(16)
+
+	if err != nil {
+		panic(err)
+	}
+
+	switch choice.Int64() {
+	case 0:
+		fmt.Println("Using ECB")
+		out = util.AESEncrypt(input, key)
+	case 1:
+		fmt.Println("Using CBC")
+		out = util.AESCBCEncrypt(input, key, []byte("\x00"))
+	}
+
+	return out
+}
+
+func main() {
+	text := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	encryptionOracle([]byte(text))
+
+}
--- util/aes.go
+++ util/aes.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"crypto/aes"
+	"fmt"
 )
 
 func AESDecrypt(data, key []byte) []byte {
@@ -20,6 +21,27 @@ func AESDecrypt(data, key []byte) []byte {
 	}
 
 	return decrypted
+}
+
+func AESEncrypt(data, key []byte) []byte {
+	cipher, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	size := cipher.BlockSize()
+	chunks := Chunkify(data, size)
+	chunks = append(chunks[:len(chunks)-1], PKCS7(chunks[len(chunks)-1], len(key)))
+	encrypted := make([]byte, len(chunks)*size)
+
+	for i, chunk := range chunks {
+		cipher.Encrypt(encrypted[i*size:(i+1)*size], chunk)
+	}
+
+	fmt.Println(encrypted)
+	fmt.Println(string(AESDecrypt(encrypted, key)))
+
+	return encrypted
 }
 
 func AESCBCDecrypt(data []byte, key []byte, iv []byte) []byte {
@@ -42,4 +64,31 @@ func AESCBCDecrypt(data []byte, key []byte, iv []byte) []byte {
 	}
 
 	return decrypted
+}
+
+func AESCBCEncrypt(data, key, iv []byte) []byte {
+	cipher, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	size := cipher.BlockSize()
+
+	data = PKCS7(data, size)
+	chunks := Chunkify(data, size)
+
+	//chunks := Chunkify(data, size)
+	//chunks = append(chunks[:len(chunks)-1], PKCS7(chunks[len(chunks)-1], len(key)))
+
+	encrypted := make([]byte, len(chunks)*size)
+
+	lastChunk := iv
+	for i, chunk := range chunks {
+		cipher.Encrypt(encrypted[i*size:(i+1)*size], RXor(chunk, lastChunk))
+		lastChunk = encrypted[i*size : (i+1)*size]
+	}
+
+	fmt.Println(encrypted)
+	fmt.Println(AESCBCDecrypt(encrypted, key, []byte("\x00")))
+	return encrypted
 }
