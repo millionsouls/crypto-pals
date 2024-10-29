b40d1dcb3b085b03fdaf51ef98e2aa125f6d9dfd
Kevin W <36448536+millionsouls@users.noreply.github.com>
2024-10-29 18:17:46 -0400

    c16

--- set2/16/main.go
+++ set2/16/main.go
@@ -2,6 +2,7 @@ package main
 
 // CBC bitflipping attack
 import (
+	"bytes"
 	"crypto-pals/util"
 	"fmt"
 	"regexp"
@@ -13,12 +14,12 @@ var setKey sync.Once
 var key []byte
 var iv []byte
 
-func wrap_data(input string) []byte {
+func wrap_data(input []byte) []byte {
 	prefix := []byte("comment1=cooking%20MCs;userdata=")
 	suffix := []byte(";comment2=%20like%20a%20pound%20of%20bacon")
 
 	re := regexp.MustCompile(`[;=]`)
-	text := []byte(re.ReplaceAllString(input, ""))
+	text := []byte(re.ReplaceAllString(string(input), ""))
 	text = append(prefix, text...)
 	text = append(text, suffix...)
 
@@ -29,6 +30,8 @@ func isAdmin(input []byte) bool {
 	dec := util.AESCBCDecrypt(input, key, iv)
 	splits := strings.Split(string(dec), ";")
 	result := make(map[string]string)
+
+	fmt.Println(string(dec))
 
 	for _, j := range splits {
 		kv := strings.Split(j, "=")
@@ -43,12 +46,28 @@ func isAdmin(input []byte) bool {
 	return result["admin"] == "true"
 }
 
+func make_admin() []byte {
+	block := bytes.Repeat([]byte("A"), 16)
+	ct := wrap_data(append(block, block...))
+
+	// fmt.Println(isAdmin(ct))
+
+	flip := util.RXor(block, append([]byte(";admin=true"), bytes.Repeat([]byte("A"), 16-len(";admin=true"))...))
+	pad := append(flip, bytes.Repeat([]byte("\x00"), 16*3-len(flip))...)
+	pad = append(pad, bytes.Repeat([]byte("\x00"), len(ct)-len(pad))...)
+	new_ct := util.RXor(ct, pad)
+
+	return new_ct
+}
+
 func main() {
 	setKey.Do(func() { key = util.GenerateRandomBytes(16); iv = util.GenerateRandomBytes(16) })
 
 	input := ";admin=true;"
-	enc := wrap_data(input)
+	enc := wrap_data([]byte(input))
 
-	isAd := isAdmin(enc)
-	fmt.Println(isAd)
+	fmt.Println(isAdmin(enc))
+
+	test := make_admin()
+	fmt.Println(isAdmin(test))
 }
